cf63439662cfc3130209e10c868a55005d2ae9d3
lprieto- <lprieto-@lprieto-U22.04vm>
2024-06-19 17:05:40 +0200

    first word different

--- rank02/n1/first_word/first_word.c
+++ rank02/n1/first_word/first_word.c
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 19:31:09 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/06/05 20:02:34 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/06/19 17:05:19 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,42 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+/*
+#include <unistd.h>
+#include <stdlib.h>
+
+int	first_word(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == 32 || str[i] == 9)
+			i++;
+		while (str[i] && str[i] != 9 && str[i] != 32)
+		{
+			write(1, &str[i], 1);
+			i++;
+			if (str[i] == 9 || str[i] == 32)
+				return (0);
+		}
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	first_word(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
+
+*/
