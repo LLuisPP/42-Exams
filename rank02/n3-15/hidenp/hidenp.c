884e8579b8303449aa2c8dedeb8080a7b6d2b451
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2024-07-02 13:52:53 +0200

    update hidenp

--- rank02/n3-15/hidenp/hidenp.c
+++ rank02/n3-15/hidenp/hidenp.c
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 16:28:50 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/06/23 16:28:53 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/07/02 12:43:48 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,37 @@ int	main(int argc, char **argv)
 	write (1, "\n", 1);
 	return (0);
 }
+
+
+/*
+
+#include <unistd.h>
+
+void	hidenp(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		if (s2[j] == s1[i])
+			i++;
+		j++;
+	}
+	if (s1[i] == '\0')
+		write(1, "1", 1);
+	else
+		write(1, "0", 1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+		hidenp(av[1], av[2]);
+	write(1, "\n", 1);
+	return (0);
+}
+
+*/
