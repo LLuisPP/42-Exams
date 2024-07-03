28d538e252731773efb26d2c8b2eb7ad3f9d7312
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2024-07-03 12:43:42 +0200

    snake_to_camel

--- rank02/n1-12/search_and_replace/search_and_replace.c
+++ rank02/n1-12/search_and_replace/search_and_replace.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   search_and_replace.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lprieto- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 08:09:15 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/06/10 08:54:46 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/07/03 10:36:50 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,3 +48,35 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+/*
+OTRA FORMA
+
+void    search_and_replace(char *str, char *find, char *rep)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == find[0])
+            write(1, &rep[0], 1);
+        else
+            write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 4 || argv[2][1] || argv[3][1])
+    {
+        write(1, "\n", 1);
+        return (0);
+    }
+    search_and_replace(argv[1], argv[2], argv[3]);
+    write(1, "\n", 1);
+    return (0);
+}
+*/
