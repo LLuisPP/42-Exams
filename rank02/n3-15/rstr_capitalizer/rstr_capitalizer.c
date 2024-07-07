b56e80ef336aeeb9ca4db8982d47b7cf6e6a27e3
Borealuis <borealuis@iMac-de-Borealuis.local>
2024-07-07 20:54:01 +0200

    rstr_capitalizer + str_cap fix

--- rank02/n3-15/rstr_capitalizer/rstr_capitalizer.c
+++ rank02/n3-15/rstr_capitalizer/rstr_capitalizer.c
@@ -1,68 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/19 18:45:50 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/07/07 20:53:06 by lprieto-         ###   ########.fr       */
+/*   Created: 2024/07/07 20:33:48 by lprieto-          #+#    #+#             */
+/*   Updated: 2024/07/07 20:51:08 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
-void	str_capitalizer(char *s)
+void	rstr_capitalizer(char *str)
 {
 	int	i;
-
+	
 	i = 0;
-	if (s[i] >= 'a' && s[i] <= 'z')
+	while (str[i])
 	{
-		s[i] -= 32;
-		write(1, &s[i], 1);
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
+			str[i] = str[i] - 32;
+		write(1, &str[i], 1);
 		i++;
 	}
-	else if (s[i] >= 'A' && s[i] <= 'Z')
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-	else
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-	while (s[i])
-	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
-			s[i] += 32;
-		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == ' ' || s[i - 1] == '\t'))
-			s[i] -= 32;
-		write(1, &s[i], 1);
-		i++;
-	}
-	write(1, "\n", 1);
+		write(1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
 
+	i = 1;
 	if (argc < 2)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	else
+	while (i < argc)
 	{
-		i = 1;
-		while (i < argc)
-		{
-			str_capitalizer(argv[i]);
-			i++;
-		}
+		rstr_capitalizer(argv[i]);
+		i++;
 	}
-	return (0);
+	return (0);	
 }
