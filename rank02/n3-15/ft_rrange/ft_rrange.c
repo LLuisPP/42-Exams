97f96ea29e1be84012f843addfdff5625b36cf24
Luis María Prieto Pedregal <lprieto-@car5s5.42barcelona.com>
2024-07-23 11:09:08 +0200

    ft_rrange revision

--- rank02/n3-15/ft_rrange/ft_rrange.c
+++ rank02/n3-15/ft_rrange/ft_rrange.c
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 10:48:46 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/07/09 12:14:36 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/07/23 11:08:25 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,4 +114,51 @@ int	main(int argc, char **argv)
 	ft_rrange(start, end);
 	return (0);	
 }
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+
+int     *ft_rrange(int start, int end)
+{
+	int	*res;
+	int	range;
+	int	i;
+
+
+	range = abs(end - start) + 1;
+	i = 0;
+	res =  (int *)malloc(sizeof(int) * range);
+	while (i < range) // mal i <= range OJO !!
+	{
+		if (start <= end)
+		{
+			res[i] = end;
+			end--;
+		}
+		else if (start >= end)
+		{
+			res[i] = start;
+			start--;
+		}
+		i++;
+	}
+	return (res);
+}
+
+/*
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	ft_rrange(atoi(argv[1]), atoi(argv[2]));
+	write(1, "\n", 1);
+	return (0);
+}*/
 
