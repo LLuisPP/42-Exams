34aab15dc7ea2342a81dacd16cd4fcd916145cec
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2024-07-24 12:23:20 +0200

    ft_rrange

--- rank02/n3-15/ft_rrange/ft_rrange.c
+++ rank02/n3-15/ft_rrange/ft_rrange.c
@@ -6,10 +6,57 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 10:48:46 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/07/23 11:08:25 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/07/24 11:57:15 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int     *ft_rrange(int start, int end)
+{
+	int	range;
+	int	i;
+	int	*res;
+
+	i = 0;
+	range = abs(end - start) + 1;
+	res = (int *)malloc(sizeof(int) * range);
+	if (!res)
+		return (NULL);
+	while (i < range)
+	{
+		if (start <= end)
+		{
+			res[i] = end;
+			end--;
+		}
+		else if (start >= end)
+		{
+			res[i] = start;
+			start++;
+		}
+		i++;
+	}
+	return (res);
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	ft_rrange(atoi(argv[1]),atoi(argv[2]));
+	write(1, "\n", 1);
+	return (0);
+}*/
+
+// -----------------------------------------------------------------
+/*
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -48,7 +95,6 @@ int     *ft_rrange(int start, int end)
 	return (range);	
 }
 
-/*
 int	main(int argc, char **argv)
 {
 	if(argc != 2)
@@ -61,7 +107,9 @@ int	main(int argc, char **argv)
 	return (0);
 }*/
 
+// ------------------------------------------------------------
 
+/*
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -114,51 +162,4 @@ int	main(int argc, char **argv)
 	ft_rrange(start, end);
 	return (0);	
 }
-
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-
-
-int     *ft_rrange(int start, int end)
-{
-	int	*res;
-	int	range;
-	int	i;
-
-
-	range = abs(end - start) + 1;
-	i = 0;
-	res =  (int *)malloc(sizeof(int) * range);
-	while (i < range) // mal i <= range OJO !!
-	{
-		if (start <= end)
-		{
-			res[i] = end;
-			end--;
-		}
-		else if (start >= end)
-		{
-			res[i] = start;
-			start--;
-		}
-		i++;
-	}
-	return (res);
-}
-
-/*
-int	main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	ft_rrange(atoi(argv[1]), atoi(argv[2]));
-	write(1, "\n", 1);
-	return (0);
-}*/
-
+*/
